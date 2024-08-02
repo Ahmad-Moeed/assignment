ed4a8da25404676e8766e0b8158f2ee564246882
Ahmad-Moeed <pilotmoeed878@gmail.com>
2024-08-02 06:15:00 +0500

    add all

---
+++ script.js
@@ -0,0 +1,15 @@
+let country= "PAKISTAN"
+let continent="ASIA"
+let population="245209815"
+
+console.log( country);
+console.log( continent);
+console.log( population);
+
+
+let island="false";
+let language="";
+console.log(island);
+console.log( country);
+console.log(population);
+console.log( language);
